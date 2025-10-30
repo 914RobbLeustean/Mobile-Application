67573a2d3c396106087cd62ee658120f1d7382bd
Leustean Robert <gabirobi59@gmail.com>
2025-10-30 23:22:20 +0200

    Initial Commit

---
+++ HabitFlow/HabitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFlowApp.swift
+//  HabitFlow
+//
+//  Created by Robert Leustean on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
